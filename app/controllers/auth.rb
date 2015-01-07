98a1f1beaf666e8d600f1ba91f081b3690060ba3
Frank DelPidio <frankdaedalus@gmail.com>
2015-01-07 09:17:49 -0500

    created authorization routes

---
+++ app/controllers/auth.rb
@@ -0,0 +1,49 @@
+get '/' do
+  p current_user
+  if current_user
+    erb :'/index'
+  else
+    erb :'auth/login', layout: false
+  end
+end
+
+get '/signup' do
+  if request.xhr?
+    erb :'auth/signup_form', layout: false
+  else
+    erb :'auth/signup_form'
+  end
+end
+
+post '/signup' do
+  user = User.new(params[:user])
+  if user.save
+    session[:user_id] = user.id
+  else
+    #display error
+  end
+  redirect '/'
+end
+
+get '/login' do
+  if request.xhr?
+    erb :'auth/login_form', layout: false
+  else
+    erb :'auth/login_form'
+  end
+end
+
+post '/login' do
+  user = User.find_by(name: params[:user][:name])
+  if user && user.authenticate(params[:user][:password])
+    session[:user_id] = user.id
+  else
+    #display error
+  end
+  redirect '/'
+end
+
+get '/logout' do
+  session[:user_id] = nil
+  redirect '/'
+end
