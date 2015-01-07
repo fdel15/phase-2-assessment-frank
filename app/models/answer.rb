ed52af2ac0e01cdeaa14047eee23277c449ea1b2
Frank DelPidio <frankdaedalus@gmail.com>
2015-01-07 12:49:54 -0500

    created answered questions helper for user

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,6 +2,11 @@ class Answer < ActiveRecord::Base
   # Remember to create a migration!
   belongs_to :question
   belongs_to :user
+
+  def answered_questions
+    self.answers.map {|ans| ans.question_id}
+  end
+
   # has_many :questions, foreign_key: "question_id", class_name: "Question"
 
 end
