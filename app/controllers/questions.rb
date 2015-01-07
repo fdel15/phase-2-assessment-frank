c6dffa65deb99dfbc50937b2079f6fefb59656cb
Frank DelPidio <frankdaedalus@gmail.com>
2015-01-07 10:41:35 -0500

    made new question form functional

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -1,3 +1,14 @@
 get '/question/new' do
   erb :'questions/new_question'
 end
+
+post '/question' do
+  params[:question][:user_id] = current_user.id
+  question = Question.new(params[:question])
+  if question.save
+    redirect '/'
+  else
+    #display error
+    redirect '/question/new'
+  end
+end
--- app/models/question.rb
+++ app/models/question.rb
@@ -3,7 +3,7 @@ class Question < ActiveRecord::Base
   belongs_to :user; :test
   has_many :answers
 
-   validates_presence_of :content, :correct_answer,
+   validates_presence_of :content, :correct_answer
 
    # validates_uniqueness_of
 
